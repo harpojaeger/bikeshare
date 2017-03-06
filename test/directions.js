3116734aff7f71671887627caeb6e69243aef0bf
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-03-05 19:34:57 -0700

    Tests for directions feature

---
+++ test/directions.js
@@ -0,0 +1,54 @@
+var app = require ('../index'),
+    supertest = require('supertest')(app),
+    chai = require('chai'),
+    expect = chai.expect
+chai.use(require('chai-json-schema'))
+
+describe('The direction getter', function(){
+  it('responds to queries without start and end addresses with 400', function(done) {
+    supertest.get('/directions')
+    .expect(400)
+    .end(done)
+  })
+
+  it('responds to queries with unresolvable addresses with 400', function(done) {
+    supertest.get('/directions')
+    .query({
+      startAddr: '1400 Shepherd St. NW, Washington DC',
+      endAddr: 'Lorem ipsum dolor sit amet'
+    })
+    .expect(400)
+    .end(done)
+  })
+
+  it('responds with JSON for all directions', function(done) {
+
+    // The JSON schema our directions must follow to be valid
+    var directionsSchema = {
+      title: 'walking & biking directions schema',
+      type: 'object',
+      properties: {
+        walking: {
+          type: 'object',
+          properties: {
+            fromOrigin: { type: 'object' },
+            toDestination: { type: 'object' },
+            required: ['fromOrigin','toDestination']
+          }
+        },
+        biking: {
+          type: 'object'
+        }
+      },
+      required: ['walking', 'biking']
+    }
+    supertest.get('/directions')
+    .query({
+      startAddr: '1400 Randolph St. NW, Washington DC',
+      endAddr: '1100 K St. NW, Washington DC'
+    })
+    .expect(200, function(err, res) {
+      expect(JSON.parse(res.text)).to.be.jsonSchema(directionsSchema)
+    })
+  })
+})
