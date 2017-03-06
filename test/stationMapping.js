0c2d874a899536d573a00d8af4bed89104908db2
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-03-06 16:30:05 -0700

    Increase station mapping test timeout

--- test/stationMapping.js
+++ test/stationMapping.js
@@ -3,7 +3,7 @@ var app = require ('../index')
 var supertest = require('supertest')(app)
 
 describe('The closest bikeshare station to', function() {
-  this.timeout(4000)
+  this.timeout(5000)
   this.slow(3000)
   describe('the shtetl', function() {
     it('is 14th & Upshur', function(done){
